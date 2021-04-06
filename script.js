58bca4ea9bb3e2f0275b09771aa65fa7692f8a9d
rafaeldellaquila <rafadellaquila@gmail.com>
2021-04-06 13:23:49 -0300

    :tada: projeto de super trunfo feito  para o curso imersão dev

---
+++ script.js
@@ -0,0 +1,379 @@
+const gandalfCard = {
+  name: "Gandalf",
+  image:
+    "https://i.pinimg.com/564x/bf/66/6f/bf666f486294a8ab281b507cdf9d8114.jpg",
+  stats: {
+    ataque: 70,
+    defesa: 50,
+    magia: 95,
+  },
+};
+
+const gimliCard = {
+  name: "Gimli",
+  image:
+    "https://i.pinimg.com/originals/2b/ed/c9/2bedc9d9106bee5e7c3f5f018019d0f6.jpg",
+  stats: {
+    ataque: 85,
+    defesa: 70,
+    magia: 20,
+  },
+};
+
+const legolasCard = {
+  name: "Legolas",
+  image:
+    "https://i.pinimg.com/originals/04/80/29/048029f362c484a2a46b928afbe98837.jpg",
+  stats: {
+    ataque: 80,
+    defesa: 40,
+    magia: 40,
+  },
+};
+
+const frodoCard = {
+  name: "Frodo",
+  image:
+    "https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2014/11/28/141128114647_frodo1.jpg",
+  stats: {
+    ataque: 50,
+    defesa: 40,
+    magia: 10,
+  },
+};
+
+const samCard = {
+  name: "Sam",
+  image:
+    "https://cdn.ome.lt/mXkGsKduF0noDBTMUc8DCXNlvrY=/1200x630/smart/extras/conteudos/Samwise.png",
+  stats: {
+    ataque: 60,
+    defesa: 40,
+    magia: 5,
+  },
+};
+
+const sauronCard = {
+  name: "Sauron",
+  image:
+    "https://i.pinimg.com/originals/6a/79/f7/6a79f7a64da318a14bd2911ebf22cd0f.jpg",
+  stats: {
+    ataque: 85,
+    defesa: 70,
+    magia: 90,
+  },
+};
+
+const lurtzCard = {
+  name: "Lurtz,  Uruk Hai",
+  image:
+    "https://i.pinimg.com/originals/a5/94/d7/a594d75c786fe7ac44fb1f2318b98e57.jpg",
+  stats: {
+    ataque: 85,
+    defesa: 50,
+    magia: 10,
+  },
+};
+
+const reiBruxoAngmarCard = {
+  name: "Rei Bruxo de Angmar",
+  image: "https://cdn.awsli.com.br/940/940250/arquivos/filme.jpg",
+  stats: {
+    ataque: 80,
+    defesa: 70,
+    magia: 50,
+  },
+};
+
+const thranduilCard = {
+  name: "Thranduil",
+  image:
+    "http://2.bp.blogspot.com/-Uo1If43yNDM/Utn-lEdsyaI/AAAAAAAAHNo/nsdAUZt3mII/s1600/hobbit-2-2.jpg",
+  stats: {
+    ataque: 70,
+    defesa: 70,
+    magia: 90,
+  },
+};
+
+const galadrielCard = {
+  name: "Galadriel",
+  image:
+    "https://i.pinimg.com/564x/4a/5c/99/4a5c991e3a8b39d3fa8ae7a67fcd9883.jpg",
+  stats: {
+    ataque: 50,
+    defesa: 40,
+    magia: 90,
+  },
+};
+
+const aragornCard = {
+  name: "Aragorn",
+  image:
+    "https://http2.mlstatic.com/D_NQ_NP_814379-MLB26433906875_112017-O.jpg",
+  stats: {
+    ataque: 85,
+    defesa: 70,
+    magia: 10,
+  },
+};
+
+const smeagolCard = {
+  name: "Smeagol",
+  image:
+    "https://i.pinimg.com/originals/08/b5/35/08b535d643221cd578eeabe5305c9a88.png",
+  stats: {
+    ataque: 30,
+    defesa: 10,
+    magia: 50,
+  },
+};
+
+const meriadocCard = {
+  name: "Meriadoc",
+  image:
+    "http://3.bp.blogspot.com/_RFpC0r8aGBY/SSFiDlxtTgI/AAAAAAAAABY/SGFm-Ayc9QQ/w1200-h630-p-k-no-nu/merry4.jpg",
+  stats: {
+    ataque: 40,
+    defesa: 10,
+    magia: 5,
+  },
+};
+
+const arwenCard = {
+  name: "Arwen",
+  image:
+    "http://pm1.narvii.com/6408/d4066e98d1a733ef78cd197777f22cd2a098abac_00.jpg",
+  stats: {
+    ataque: 30,
+    defesa: 40,
+    magia: 85,
+  },
+};
+
+const eowynCard = {
+  name: "Éowyn",
+  image:
+    "https://i.pinimg.com/originals/bc/47/7d/bc477d70d2984cd3c1f9cb40335cc70b.jpg",
+  stats: {
+    ataque: 85,
+    defesa: 60,
+    magia: 50,
+  },
+};
+
+const sarumanCard = {
+  name: "Saruman",
+  image:
+    "https://i.pinimg.com/originals/6d/b3/3f/6db33fc804f7059bc0e7a0226f845664.jpg",
+  stats: {
+    ataque: 30,
+    defesa: 50,
+    magia: 80,
+  },
+};
+
+const balrogCard = {
+  name: "Balrog",
+  image:
+    "https://pm1.narvii.com/6929/e5e6d1de113ca75d3bd832d6447f5680e0884542r1-466-659v2_hq.jpg",
+  stats: {
+    ataque: 90,
+    defesa: 70,
+    magia: 80,
+  },
+};
+
+const boromirCard = {
+  name: "Boromir",
+  image:
+    "https://comicvine1.cbsistatic.com/uploads/original/11113/111134509/3496930-7724158094-32095.jpg",
+  stats: {
+    ataque: 70,
+    defesa: 70,
+    magia: 20,
+  },
+};
+
+const bilboCard = {
+  name: "Bilbo",
+  image:
+    "https://i.pinimg.com/originals/7e/bb/3f/7ebb3f6e8b8b20e8f29c657dbc67870f.jpg",
+  stats: {
+    ataque: 60,
+    defesa: 40,
+    magia: 40,
+  },
+};
+const eomerCard = {
+  name: "Éomer",
+  image:
+    "http://pm1.narvii.com/6416/1f5f4fcac41a97f9842057ebfae2fa4e98d6632e_00.jpg",
+  stats: {
+    ataque: 70,
+    defesa: 50,
+    magia: 10,
+  },
+};
+const cards = [
+  gandalfCard,
+  gimliCard,
+  legolasCard,
+  frodoCard,
+  samCard,
+  sauronCard,
+  lurtzCard,
+  reiBruxoAngmarCard,
+  thranduilCard,
+  galadrielCard,
+  aragornCard,
+  smeagolCard,
+  meriadocCard,
+  arwenCard,
+  eowynCard,
+  sarumanCard,
+  balrogCard,
+  boromirCard,
+  bilboCard,
+  eomerCard,
+];
+
+let playerScore = 0;
+let iaScore = 0;
+
+updateScoreboard();
+updateCards();
+
+function updateScoreboard() {
+  const divScoreboard = document.getElementById("placar");
+  const scoreBoard = "Jogador " + playerScore + " / " + iaScore + " Máquina";
+
+  divScoreboard.innerHTML = scoreBoard;
+}
+
+function updateCards() {
+  const divCardsInGame = document.getElementById("quantidade-cartas");
+  let cardsInGame = "Quantidade de cartas no jogo: " + cards.length;
+
+  divCardsInGame.innerHTML = cardsInGame;
+}
+
+function drawCards() {
+  let iaCardNumber = parseInt(Math.random() * cards.length);
+  iaCard = cards[iaCardNumber];
+  cards.splice(iaCardNumber, 1);
+
+  let playerCardNumber = parseInt(Math.random() * cards.length);
+  playerCard = cards[playerCardNumber];
+  cards.splice(playerCardNumber, 1);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  showPlayerCard();
+}
+
+function showPlayerCard() {
+  const divPlayerCard = document.getElementById("carta-jogador");
+  const cardTemplate = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png">`;
+  const nameCard = `<p class="carta-subtitle">${playerCard.name}</p>`;
+  let textOptions = "";
+  const divStats = `<div id="opcoes" class="carta-status">`;
+
+  for (let stat in playerCard.stats) {
+    textOptions +=
+      "<input type='radio' name='atributo' value='" +
+      stat +
+      "'>" +
+      stat +
+      " " +
+      playerCard.stats[stat] +
+      "<br>";
+  }
+
+  divPlayerCard.style.backgroundImage = `url(${playerCard.image})`;
+  divPlayerCard.innerHTML =
+    cardTemplate + nameCard + divStats + textOptions + " </div>";
+}
+
+function getStat() {
+  const statRadio = document.getElementsByName("atributo");
+  for (let i = 0; i < statRadio.length; i++) {
+    if (statRadio[i].checked) {
+      return statRadio[i].value;
+    }
+  }
+}
+
+function play() {
+  const selectedStat = getStat();
+  const divScore = document.getElementById("resultado");
+
+  if (playerCard.stats[selectedStat] > iaCard.stats[selectedStat]) {
+    finalScore = "<p class='resultado-final'>Venceu a carta da máquina</p>";
+    playerScore++;
+  } else if (playerCard.stats[selectedStat] < iaCard.stats[selectedStat]) {
+    finalScore =
+      "<p class='resultado-final'>Perdeu para a carta da máquina</p>";
+    iaScore++;
+  } else {
+    finalScore = "<p class='resultado-final'>Empatou</p>";
+  }
+
+  if (cards.length == 0) {
+    alert("Fim de jogo.");
+    if (playerScore > iaScore) {
+      finalScore = "<p class='resultado-final'>Venceu a máquina</p>";
+    } else if (playerScore < iaScore) {
+      finalScore = "<p class='resultado-final'>Perdeu para a máquina</p>";
+    } else {
+      finalScore = "<p class='resultado-final'>Vocês empataram</p>";
+    }
+  } else {
+    document.getElementById("btnProximaRodada").disabled = false;
+  }
+
+  divScore.innerHTML = finalScore;
+  document.getElementById("btnJogar").disabled = true;
+
+  updateScoreboard();
+  showIaCard();
+  updateCards();
+}
+
+function showIaCard() {
+  const divIaCard = document.getElementById("carta-maquina");
+  const cardTemplate = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png">`;
+  const nameCard = `<p class="carta-subtitle">${iaCard.name}</p>`;
+  let textOptions = "";
+  const divStats = `<div id="opcoes" class="carta-status">`;
+
+  for (let stat in iaCard.stats) {
+    textOptions +=
+      "<p type='text' name='atributo' value='" +
+      stat +
+      "'>" +
+      stat +
+      " " +
+      iaCard.stats[stat] +
+      "</p>";
+  }
+  divIaCard.style.backgroundImage = `url(${iaCard.image})`;
+  divIaCard.innerHTML =
+    cardTemplate + nameCard + divStats + textOptions + " </div>";
+}
+
+function nextRound() {
+  const divCards = document.getElementById("cartas");
+  divCards.innerHTML = `
+  <div id="carta-jogador" class="carta"></div>
+  <div id="carta-maquina" class="carta"></div>
+  `;
+
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btjnJogar").disabled = true;
+  document.getElementById("btnProximaRodada").disabled = true;
+
+  const divScore = document.getElementById("resultado");
+  divScore.innerHTML = "";
+}
